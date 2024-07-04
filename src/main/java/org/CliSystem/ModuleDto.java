94b0e08171f8839175cc7f30124be43bf97b3d49
Ибатов Эрнест <117213736+re1ta@users.noreply.github.com>
2024-07-04 18:10:32 +0500

    try1

--- src/main/java/org/CliSystem/ModuleDto.java
+++ src/main/java/org/CliSystem/ModuleDto.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Map;
 
 public record ModuleDto(
-        @JsonProperty(value = "name") String name,
-        @JsonProperty(value = "metadata") Map<String, String> metadata
+        String name,
+        Map<String, String> metadata
 ) {
 }
--- src/main/java/org/CliSystem/ModuleObj.java
+++ src/main/java/org/CliSystem/ModuleObj.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Map;
 
 public record ModuleObj(
-        @JsonProperty(value = "name") String name,
-        @JsonProperty(value = "script") String script,
-        @JsonProperty(value = "metadata") Map<String, String> metadata) {
+        String name,
+        String script,
+        Map<String, String> metadata) {
 }
